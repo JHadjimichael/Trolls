df375f150193ea5b10a2fee9264a2ab5bd7a52a9
JHadjimichael <jordan.hadjimichael@gmail.com>
2021-04-28 10:53:39 -0400

    Driver and Unfinished panel

---
+++ Stubs/TrollDriver.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import javax.swing.JOptionPane;
+public class TrollDriver
+{
+   public static void main(String[] args) throws Exception
+   {
+      //Set Panel to TrollPanel
+   }
+ 
+}
---
+++ Stubs/TrollPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class TrollPanel extends JPanel
+{
+   private JLabel label1, label2;
+   private JTextField box;
+   private int number, count, its=0;
+   public Panel03()
+   {
+      //fill
+   }
+}
